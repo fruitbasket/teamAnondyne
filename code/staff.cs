20dd97c138d163c352491c74e65d39f9a67150a5
harry4696 <harry4696@gmail.com>
2013-11-13 18:48:29 +0000

    the classes i coded today

---
+++ code/staff.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Staff
+    {
+
+    //Staff variables
+        private int staffID = 0;
+        private string firstName;
+        private string lastName;
+        private string staffRole;
+        private string Availbility;
+
+    // Staff Properties
+        public int _staffID 
+        { 
+            get 
+                {
+                    return staffID;
+                }
+            set 
+                {
+                    staffID = value;
+                }
+        }
+
+        public string _firstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string _lastName
+        {
+            get
+            {
+               return lastName;
+            }
+            set
+            {
+               lastName = value;
+            }
+        }
+
+        public string _staffRole
+        {
+            get
+            {
+                return staffRole;
+            }
+            set
+            {
+                staffRole = value;
+            }
+        }
+
+        public string _Availbility
+        {
+            get
+            {
+                return Availbility;
+            }
+            set
+            {
+                 Availbility= value;
+            }
+        }
+
+    // Staff methods
+    public void staffSearch()
+    {
+    
+    
+    }
+
+    }
+}
